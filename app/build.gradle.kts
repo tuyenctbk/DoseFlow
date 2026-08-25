ec9a196a80221c2b3feefefc619b41928fce356e
Tuyen <tuyenctbk@gmail.com>
2026-08-25 19:10:50 +0700

    chore: upgrade Gradle wrapper to 9.6.1 and update project version to 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
     applicationId = "com.soloprono.doseflow"
     minSdk = 26
     targetSdk = 36
-    versionCode = 6
-    versionName = "1.5"
+    versionCode = 7
+    versionName = "1.6"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
